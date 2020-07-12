b188e2012b8c92b466a927b07f07119f83f073cd
TheHaunted <kevin_lopez@live.com>
2020-07-11 22:30:26 -0500

    adds sessions

--- api/server.js
+++ api/server.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const server = express();
-require('dotenv').config()
+const session = require('express-session');
+const helmet = require('helmet');
+require('dotenv').config();
 
 const productsRouter = require('./products/products-router');
 const usersRouter = require('./users/users-router');
 
+const sessionConfig = {
+    name: 'Banana',
+    secret: process.env.SECRET,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
+        secure: false, // true for production
+        httpOnly: true
+    },
+    resave: false,
+    saveUninitialized: false
+}
+
+server.use(helmet);
 server.use(express.json());
+server.use(session(sessionConfig))
 
 server.use('/products', productsRouter);
 server.use('/users', usersRouter);
